3e91469390434e60c516db2b3198698617cca697
Jan Cichy <04cichy@gmail.com>
2024-03-20 20:02:00 +0100

    init

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello World\n");
+
+//     return 0;
+// }
+int main()
+{
+    sfVideoMode mode = {800, 450, 32};
+    sfRenderWindow *window = sfRenderWindow_create(mode, "CSFML Example", sfResize | sfClose, NULL);
+
+    sfRectangleShape *shape = sfRectangleShape_create();
+    sfVector2f vf = {200, 200}, pos = {30, 30};
+    sfRectangleShape_setSize(shape, vf);
+    sfRectangleShape_setFillColor(shape, sfGreen);
+    sfRectangleShape_setPosition(shape, pos);
+
+    while (sfRenderWindow_isOpen(window))
+    {
+        sfEvent event;
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+            if (event.type == sfEvtClosed)
+            {
+                sfRenderWindow_close(window);
+            }
+        }
+
+        sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_drawRectangleShape(window, shape, NULL);
+        sfRenderWindow_display(window);
+    }
+
+    sfRectangleShape_destroy(shape);
+    sfRenderWindow_destroy(window);
+
+    return 0;
+}
