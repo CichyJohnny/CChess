02a21429c6b69fa6073b09f4aee149312dc39e64
Jan Cichy <04cichy@gmail.com>
2024-03-30 20:36:18 +0100

    implementation of chessboard

--- main.c
+++ main.c
@@ -1,89 +1,79 @@
-#include "game.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <SFML/Graphics.h>
 
-void spawnNSquare(window, square, n) {
-    time_t t;
-    int winx = sfWindow_getSize(window).x;
-    int sqrx = sfRectangleShape_getSize(square).x;
-    int winy = sfWindow_getSize(window).y;
-    int sqry = sfRectangleShape_getSize(square).y;
-    srand((unsigned) time(&t));
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define SQUARE_SIZE 100
 
-    for (int i = 0; i<n; i++){
-        int posx = rand() % (winx - sqrx);
-        int posy = rand() % (winy - sqry);
-        sfVector2f pos = {posx, posy};
-        sfRectangleShape_setPosition(square, pos);
+sfRectangleShape* createSquare(sfVector2f position, sfVector2f size, sfColor color) {
+    sfRectangleShape* square = sfRectangleShape_create();
+    sfRectangleShape_setPosition(square, position);
+    sfRectangleShape_setSize(square, size);
+    sfRectangleShape_setFillColor(square, color);
+    return square;
+}
 
-        sfRenderWindow_drawRectangleShape(window, square, NULL);
+void drawChessboard(sfRenderWindow* window, sfRectangleShape* squares[8][8]) {
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            sfRenderWindow_drawRectangleShape(window, squares[i][j], NULL);
+        }
     }
 }
 
+int main() {
+    sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
+    sfRenderWindow* window = sfRenderWindow_create(mode, "Chessboard", sfDefaultStyle, NULL);
+    sfRenderWindow_setFramerateLimit(window, 60);
 
-int main()
-{
-    // Window
-    sfVideoMode mode = {800, 450, 32};
-    sfRenderWindow *window = sfRenderWindow_create(mode, "CSFML Example", sfTitlebar | sfClose, NULL);
-    sfWindow_setFramerateLimit(window, 10);
+    sfRectangleShape* squares[8][8];
 
-    // Rectangle
-    sfRectangleShape *shape = sfRectangleShape_create();
-    sfVector2f vf = {50, 50}, pos = {30, 30};
-    sfRectangleShape_setSize(shape, vf);
-    sfRectangleShape_setFillColor(shape, sfWhite);
-    sfRectangleShape_setOutlineColor(shape, sfBlack);
-    sfRectangleShape_setOutlineThickness(shape, 10);
-    sfRectangleShape_setPosition(shape, pos);
-
-
-    // Multiple rectangles
-    sfRectangleShape *shapes[8];
-    int num_shapes = sizeof(shapes) / sizeof(shapes[0]);
-    printf("%d\n", num_shapes);
-    for (int i = 0; i < num_shapes; i++) {
-        sfRectangleShape *shape = sfRectangleShape_create();
-        sfVector2f vf = {50, 50}, pos = {30 + i*60, 30};
-        sfRectangleShape_setSize(shape, vf);
-        sfRectangleShape_setFillColor(shape, sfRed);
-        sfRectangleShape_setOutlineColor(shape, sfBlue);
-        sfRectangleShape_setOutlineThickness(shape, 2);
-        sfRectangleShape_setPosition(shape, pos);
-
-        shapes[i] = shape;
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            sfVector2f position = {j * SQUARE_SIZE, i * SQUARE_SIZE};
+            sfVector2f size = {SQUARE_SIZE, SQUARE_SIZE};
+            sfColor color;
+            if ((i + j) % 2 == 0)
+                color = sfColor_fromRGB(209, 139, 71); // Brown color for squares
+            else
+                color = sfColor_fromRGB(255, 206, 158); // Cream color for squares
+            squares[i][j] = createSquare(position, size, color);
+        }
     }
 
-
-    // Game loop
-    while (sfRenderWindow_isOpen(window))
-    {
+    while (sfRenderWindow_isOpen(window)) {
         sfEvent event;
-        while (sfRenderWindow_pollEvent(window, &event))
-        {
-            if (event.type == sfEvtClosed)
-            {
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            if (event.type == sfEvtClosed) {
                 sfRenderWindow_close(window);
             }
-            if (event.type == sfEvtKeyPressed) {
+            else if (event.type == sfEvtKeyPressed) {
                 if (event.key.code == sfKeyEscape) {
                     sfRenderWindow_close(window);
                 }
             }
+            else if (event.type == sfEvtMouseButtonPressed) {
+                if (event.mouseButton.button == sfMouseLeft) {
+                    // Get the position of the mouse click
+                    sfVector2i mousePos = {event.mouseButton.x, event.mouseButton.y};
+                    // Check which square was clicked and make it disappear
+                    int col = mousePos.x / SQUARE_SIZE;
+                    int row = mousePos.y / SQUARE_SIZE;
+                    if (col >= 0 && col < 8 && row >= 0 && row < 8) {
+                        sfRectangleShape_setSize(squares[row][col], (sfVector2f){0, 0});
+                    }
+                }
+            }
         }
 
-        // Render
         sfRenderWindow_clear(window, sfWhite);
-
-        spawnNSquare(window, shape, 10);
-
-        
+        drawChessboard(window, squares);
         sfRenderWindow_display(window);
     }
 
-    for (int i = 0; i < num_shapes; i++) {
-        sfRectangleShape_destroy(shapes[i]);
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            sfRectangleShape_destroy(squares[i][j]);
+        }
     }
 
     sfRenderWindow_destroy(window);
